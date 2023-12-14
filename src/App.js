3583279f12b0646d0c008e131773fc167afc80c5
Rahman <afsana.rahman@fiserv.com>
2023-12-14 09:50:04 -0500

    dependency bug fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ViewAllTenantInfo from './components/ViewAllTenantInfo';
 
 function App() {
   return (
     <div className="App">
       <h1>griddle</h1>
       <BrowserRouter>
-        <Route></Route>
+      <Routes>
+        <Route path={"/tenant"} element={<ViewAllTenantInfo/>} exact={true}></Route>
+      </Routes>
       </BrowserRouter>
     </div>
   );
