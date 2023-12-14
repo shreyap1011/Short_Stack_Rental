aedfc21bfb23ca45444bb40bae5713cc55ab4b0a
Drew-Huth <drew.huth@fiserv.com>
2023-12-14 16:35:11 -0500

    Building and User Edits

---
+++ src/main/java/com/shortstack/griddle/model/Building.java
@@ -0,0 +1,70 @@
+package com.shortstack.griddle.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Building {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer buildingID;
+    private Integer addressID;
+    private Integer userID;
+    private String buildingName;
+    
+    public Building() {
+    }
+
+    public Building(Integer buildingID, Integer addressID, Integer userID, String buildingName) {
+        this.buildingID = buildingID;
+        this.addressID = addressID;
+        this.userID = userID;
+        this.buildingName = buildingName;
+    }
+
+    public Integer getBuildingID() {
+        return buildingID;
+    }
+
+    public void setBuildingID(Integer buildingID) {
+        this.buildingID = buildingID;
+    }
+
+    public Integer getAddressID() {
+        return addressID;
+    }
+
+    public void setAddressID(Integer addressID) {
+        this.addressID = addressID;
+    }
+
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public String getbuildingName() {
+        return buildingName;
+    }
+
+    public void setbuildingName(String buildingName) {
+        this.buildingName = buildingName;
+    }
+
+    @Override
+    public String toString() {
+        return "Building {" + 
+        "buildingID=" + buildingID + 
+        ", addressID=" + addressID + 
+        ", userID=" + userID + 
+        ", buildingName=" + buildingName +
+         "}";
+    }
+
+}
+
--- src/main/java/com/shortstack/griddle/model/User.java
+++ src/main/java/com/shortstack/griddle/model/User.java
@@ -7,7 +7,6 @@ import jakarta.persistence.Id;
 
 @Entity
 public class User {
-     // FIELDS
     // @id means id will auto generate
 
     @Id
