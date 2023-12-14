753c0a957585d30a954ab8ca0953f90afe0d6a9e
Drew-Huth <drew.huth@fiserv.com>
2023-12-14 16:19:17 -0500

    User and AppProperties

--- src/main/java/com/shortstack/griddle/model/User.java
+++ src/main/java/com/shortstack/griddle/model/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
+@Entity
 public class User {
      // FIELDS
     // @id means id will auto generate
