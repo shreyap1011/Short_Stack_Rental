a6bcf1af91cd5037518894b6d82cb09dc49dca89
Drew-Huth <drew.huth@fiserv.com>
2023-12-14 15:48:37 -0500

    project initial setup

---
+++ src/main/java/com/shortstack/griddle/GriddleApplication.java
@@ -0,0 +1,13 @@
+package com.shortstack.griddle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GriddleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GriddleApplication.class, args);
+	}
+
+}
