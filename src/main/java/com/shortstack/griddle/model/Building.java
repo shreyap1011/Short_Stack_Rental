44b0c8fc72c015f4aefb1b067eee32b5f1f60c81
Drew-Huth <drew.huth@fiserv.com>
2023-12-14 16:41:14 -0500

    Address and Building edits

--- src/main/java/com/shortstack/griddle/model/Building.java
+++ src/main/java/com/shortstack/griddle/model/Building.java
@@ -58,7 +58,7 @@ public class Building {
 
     @Override
     public String toString() {
-        return "Building {" + 
+        return "Building{" + 
         "buildingID=" + buildingID + 
         ", addressID=" + addressID + 
         ", userID=" + userID + 
