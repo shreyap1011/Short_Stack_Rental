7338b68d362a06c64be85cca6dcb39d264966576
Shreya Patolia <shreyap3149@gmail.com>
2023-12-27 16:15:02 -0500

    authen

--- src/App.js
+++ src/App.js
@@ -19,6 +19,9 @@ import AddApartment from './components/landlord/building/AddApartment';
 import AddLease from './components/landlord/building/AddLease';
 import BalanceOverview from './components/landlord/BalanceOverview';
 import ViewTenant from './components/landlord/ViewTenant';
+import TenantService from './service/TenantService';
+import RequireAuth from './components/RequireAuth';
+import Layout from './components/Layout';
 
 const Header = () =>{
   const navigate = useNavigate();
@@ -37,28 +40,32 @@ function App() {
   return (
     <div className="App">
       {/* <img src="./img/logo.png" alt="griddle"/> */}
-      <BrowserRouter>
-
       <Header />
       <Routes>
-          <Route path={"/"} element={<LandingPage/>} exact={true}/>
-          <Route path={"/login"} element={<LoginPage/>} exact={true}/>
-          <Route path={"/registration"} element={<RegistrationPage/>}/>
-          <Route path={"/landlordlogin"} element={<LandlordLoginPage/>} exact={true}/>
-          <Route path={"/landlordregistration"} element={<LandlordRegistrationPage/>}/>
-          <Route path={"/landlord"} element={<ViewAllLandlordInfo />} exact={true}/>
-          <Route path={"/landlord/balanceOverview"} element={<BalanceOverview />} exact={true}/>
-          <Route path={"/landlord/addTenant"} element={<AddTenant/>} exact={true}/>
-          <Route path={"/landlord/addBuilding"} element={<AddBuilding/>} exact={true}/>
-          <Route path={"/landlord/addApartment"} element={<AddApartment/>} exact={true}/>
-          <Route path={"/landlord/addLease"} element={<AddLease/>} exact={true}/>
-          <Route path={"/landlord/viewBuilding"} element={<ViewTenantsForBuilding/>} exact={true}/>
-          <Route path={"/landlord/viewTenant"} element={<ViewTenant/>} exact={true}/>
-          <Route path={"/tenant/dashboard"} element={<ViewAllTenantInfo />} exact={true}/>
-          <Route path={"/tenant/paymentHistory"} element={<ViewPaymentHistory/>} exact={true}></Route>
-          <Route path={"/tenant/newPayment"} element={<AddCard/>} exact={true}></Route>
+          <Route path="/" element={<Layout />}>
+              <Route path={"/"} element={<LandingPage/>} exact={true}/>
+              <Route path={"/login"} element={<LoginPage/>} exact={true}/>
+              <Route path={"/registration"} element={<RegistrationPage/>}/>
+              <Route path={"/landlordlogin"} element={<LandlordLoginPage/>} exact={true}/>
+              <Route path={"/landlordregistration"} element={<LandlordRegistrationPage/>}/>
+              {/* <Route element={<TenantService/>}/> */}
+          
+              <Route element={<RequireAuth/>}>
+                <Route path={"/landlord"} element={<ViewAllLandlordInfo />} exact={true}/>
+                <Route path={"/landlord/balanceOverview"} element={<BalanceOverview />} exact={true}/>
+                <Route path={"/landlord/addTenant"} element={<AddTenant/>} exact={true}/>
+                <Route path={"/landlord/addBuilding"} element={<AddBuilding/>} exact={true}/>
+                <Route path={"/landlord/addApartment"} element={<AddApartment/>} exact={true}/>
+                <Route path={"/landlord/addLease"} element={<AddLease/>} exact={true}/>
+                <Route path={"/landlord/viewBuilding"} element={<ViewTenantsForBuilding/>} exact={true}/>
+                <Route path={"/landlord/viewTenant"} element={<ViewTenant/>} exact={true}/>
+                <Route path={"/tenant/dashboard"} element={<ViewAllTenantInfo />} exact={true}/>
+                <Route path={"/tenant/paymentHistory"} element={<ViewPaymentHistory/>} exact={true}></Route>
+                <Route path={"/tenant/newPayment"} element={<AddCard/>} exact={true}></Route>
+              </Route>
+
+          </Route>
       </Routes>
-      </BrowserRouter>
     </div>
   );
 }
--- src/service/TenantService.jsx
+++ src/service/TenantService.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
+import useAuth from "../hooks/useAuth";
 
 const getAllTenantsUrl = "http://localhost:8080/api/tenants";
 const getAllTenantsByLandlordUrl = "http://localhost:8080/api/tenants/landlord/";
@@ -11,40 +12,53 @@ const addTenantUrl = "http://localhost:8080/api/addTenant";
 const updateTenantUrl = "http://localhost:8080/api/updateTenant";
 const deleteTenantUrl = "http://localhost:8080/api/deleteTenant/";
 
+
 class TenantService {
-    getAllTenants() {
+    getAllTenants(accessToken) {
         return axios.get(getAllTenantsUrl);
     }
     
-    getAllTenantsByLandlord(id) {
+    getAllTenantsByLandlord(id, accessToken) {
         return axios.get(getAllTenantsByLandlordUrl + id);
     }
 
-    findTenant(id) {
+    findTenant(id, accessToken) {
         return axios.get(findTenantUrl + id);
     }
 
-    findBuildingByTenant(id) {
+    findBuildingByTenant(id, accessToken) {
         return axios.get(findBuildingByTenantUrl + id);
     }
 
-    findApartmentByTenant(id) {
-        return axios.get(findApartmentByTenantUrl + id);
+    findApartmentByTenant(id, accessToken) {
+        console.log("service: " + id);
+        return axios.get(findApartmentByTenantUrl + id, {
+            headers: {
+                Authorization : `Bearer ${accessToken}`
+            }, });
     }
 
-    findTenantByUsername(username) {
-        return axios.get(findTenantByUsernameUrl, {params : {username : username}})
+    async findTenantByUsername(username, accessToken) {
+        // const { auth } = useAuth();
+        // return axios.get(findTenantByUsernameUrl, {params : {username : username}});
+        return await axios.get(findTenantByUsernameUrl, {
+            headers: {
+                Authorization : `Bearer ${accessToken}`
+            }, 
+            params : {username : username}
+        },
+    );
     }
 
-    addTenant(tenant) {
+    addTenant(tenant, accessToken) {
         return axios.post(addTenantUrl, tenant);
     }
 
-    updateTenant(tenant) {
+    updateTenant(tenant, accessToken) {
         return axios.put(updateTenantUrl, tenant);
     }
 
-    deleteTenant(id) {
+    deleteTenant(id, accessToken) {
         return axios.delete(deleteTenantUrl + id);
     }
 }
